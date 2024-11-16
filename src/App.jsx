bd3c5c6d7b36784141cfad7d1dc6011fd6266d06
Kalugy <dam0171@utp.edu.co>
2024-11-16 13:33:57 -0500

    new header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Features from './components/Features';
 import Footer from './components/Footer';
 import CalendarPage from './components/Calendar/CalendarPage';
 import FAQs from "./components/FAQ/FAQ";
-import Review from "./components/Review/Review";
+//import Review from "./components/Review/Review";
 
 function App() {
 
