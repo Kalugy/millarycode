e998eb15434da324f4cfa7e1934c415c17449c36
Kalugy <dam0171@utp.edu.co>
2024-11-23 12:35:02 -0500

    starting new project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => (
 
       <img src="/images/test.png" alt="Logo" className="h-12" />
 
-      <h1 className="text-3xl font-bold ml-2">GROWVYN</h1>
+      <h1 className="text-3xl font-bold ml-2">Growvyn</h1>
       {/* <nav>
         <a href="#features" className="mx-4 text-white hover:text-green-300">Features</a>
         <a href="#contact" className="mx-4 text-white hover:text-green-300">Contact</a>
@@ -18,14 +18,17 @@ const Header = () => (
 
     <div className="flex-grow flex items-center justify-center text-center">
       <div>
-        <h2 className="text-5xl md:text-6xl font-extrabold mb-4">
+        <h1 className="text-5xl md:text-6xl font-extrabold mb-4 collapse">
+          Growvyn
+        </h1>
+        <h2 className="text-5xl md:text-6xl font-extrabold">
           Track Time. Grow Wisely.
         </h2>
         <p className="text-lg md:text-xl mb-8">
           Take control of your days with <strong className="">Growvyn</strong>. Start building better habits today.
         </p>
         <button className="bg-white text-blue-600 font-bold px-6 py-3 rounded-full hover:bg-blue-100 transition duration-300">
-          Get Started
+          Get Started!
         </button>
       </div>
     </div>
