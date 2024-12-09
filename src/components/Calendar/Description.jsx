c04343d26b7c3b3eadf2744bf3ba46edd9ad5ee1
Kalugy <dam0171@utp.edu.co>
2024-12-09 18:03:45 -0500

    fix padding on cellphone on description calendar

--- src/components/Calendar/Description.jsx
+++ src/components/Calendar/Description.jsx
@@ -1,6 +1,6 @@
 const Description = () => {
     return (
-      <div className="p-6 ml-8 flex flex-col gap-5 items-center md:items-start sm:flex-1 md:w-2/3 lg:w-1/2 xl:w-1/">
+      <div className="p-1 pb-8 md:pb-2 md:p-6 ml-2 md:ml-8 flex flex-col gap-5 items-center md:items-start sm:flex-1 md:w-2/3 lg:w-1/2 xl:w-1/">
         <h1 className="flex gap-4 text-2xl sm:text-3xl md:text-4xl font-bold mb-4">
           Control of Your Time
         </h1>
