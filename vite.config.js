9ea03ff40405906d9ba1e3dec3143813f43f1cde
Kalugy <dam0171@utp.edu.co>
2025-04-07 19:12:53 -0500

    update landing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
-    Sitemap({
-      hostname: 'https://growvyn.com/', // Replace with your actual domain
-      routes: ['/'], // Only the root route
-    }),
   ],
 });
